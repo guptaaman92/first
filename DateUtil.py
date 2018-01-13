9624716e45b607a4e0620653ea767887148d7200
Aman Gupta <guptaaman92@hotmail.com>
2018-01-13 16:38:56 +0530

    Added 2 moe functions for timestamp and current Month

--- DateUtil.py
+++ DateUtil.py
@@ -1,3 +1,7 @@
 # a function that will return only current date
 
 # a function that will return current year
+
+# a function that wil return the datetime
+
+# a function taht will return the current month
